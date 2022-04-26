11ac30ddc90bafaa137a96c028924c0c55a90c89
Markos Dhimiter Verdhi <90333866+Markosverdhi@users.noreply.github.com>
2022-04-26 16:30:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,29 +21,52 @@ for col in troposphere.iter_rows(min_row=2, max_col=7, max_row=519, values_only=
 for col in stratosphere.iter_rows(min_row=2, max_col=7, max_row=519, values_only=True):
     stratoArr.append(col)
 
+#modified binary search method for openpyxl
+def findInterval(arr, startMonth, startYear, endMonth, endYear, r=len(arr), l=0):
+    returns = []
+    while r-l > 1:
+        middle = (l+(r-l))//2
+        if arr[middle][0] == startYear:
+            if startMonth == l:
+                returns.append(middle)
+            elif startMonth != l:
+                diff = startMonth - arr[middle][1]
+                returns.append(middle + diff)
+            monthdiff = endMonth - startMonth
+            yeardiff = endYear - startYear
+            endIndex = returns[0] + (12*yeardiff + monthdiff)
+            returns.append(endIndex)
+            return returns
+
+        elif arr[middle][0] > startYear:
+            findInterval(arr, startMonth, startYear, endMonth, endYear, r, middle)
+        elif arr[middle][0] < startYear:
+            findInterval(arr,startMonth,startYear,endMonth,endYear,middle,l)
+        else:
+            print('one or more entries in specified range do not exist in the data')
+            return None
+            
 # method for graphing data
-def graphShit(arr, startMonth, startYear, endMonth, endYear):
+def graphShit(arr, startMonth, startYear, endMonth, endYear, hemi):
     fig, ax = plt.subplots()
-    if endYear - startYear <= 2:
-        ax.plot(xsummer, ysummer, color = 'red')
-        # ax.plot(xwinter, ywinter, color = 'blue')
-        # ax.plot(xspring, yspring, color = 'cyan')
-        # ax.plot(xfall, yfall, color = 'orange')
-        # ax.set(xlabel = "year",
-        #        ylabel = 'temp anomaly')
-        # plt.show()
 
-fig, ax = plt.subplots()
-#logic test run
-xsummer = []
-ysummer = []
-xwinter = []
-ywinter = []
-xspring = []
-yspring = []
-xfall = []
-yfall = []
+    ax.plot(xvalues, yvalues)
+    plt.show()
+    print(xvalues)
+    print(yvalues)
+graphShit(tropoArr, 1, 2000, 3, 2000, 2)
 
+# fig, ax = plt.subplots()
+# #logic test run
+# xsummer = []
+# ysummer = []
+# xwinter = []
+# ywinter = []
+# xspring = []
+# yspring = []
+# xfall = []
+# yfall = []
+#
 # for entry in tropoArr:
 #     if entry[1] == (12 or 1 or 2):
 #         ywinter.append(entry[2])
